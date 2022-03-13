1672c997b9232132c45441d34193712c80a61e59
jackytld <95678364+jackytld@users.noreply.github.com>
2022-03-13 19:27:43 +0100

    menuer + js

--- badges.js
+++ badges.js
@@ -1,12 +1,22 @@
 console.log("java.js er fungerende...")
 
-function myFunction() {
+let unlockedbadges = ["badge1", "badge2", "badge3", "badge4"];
+let lockedbadges = ["badge5", "badge6", "badge7", "badge8"];
+
+var output=document.getElementById("out");
+if (unlockedbadges[2]=="badge3"){
+    alert ("yes")
+    output.innerHTML="yes";
+}else{
+    alert ("no")
+    output.innerHTML="no";
+}
+
+/*function myFunction() {
     let badgetoggle = document.getElementById("badge");
     if (badgetoggle.style.display === "none") {
         badgetoggle.style.display = "block";
       } else {
         badgetoggle.style.display = "none";
       }
-    }
-
-var unlockedbadges = ["badge1", "badge2", "badge3"];
+    }*/
