b37ed416ad55dc164f9e95daf2ef42a1cb026268
jackytld <95678364+jackytld@users.noreply.github.com>
2022-03-14 10:48:00 +0100

    badges

--- MAKK-Tabs/badges.js
+++ MAKK-Tabs/badges.js
@@ -1,25 +1,15 @@
-console.log("java.js er fungerende...")
-
 let unlockedbadges = ["badge1", "badge2", "badge3", "badge4"];
 let lockedbadges = ["badge5", "badge6", "badge7", "badge8"];
 
 var output=document.getElementById("out");
 if (unlockedbadges[2]=="badge3"){
-    alert ("yes")
-    output.innerHTML="yes";
+    console.log ("yes")
 }else{
-    alert ("no")
-    output.innerHTML="no";
+    console.log ("no")
 }
 
-/*function myFunction() {
-    let badgetoggle = document.getElementById("badge");
-    if (badgetoggle.style.display === "none") {
-        badgetoggle.style.display = "block";
-      } else {
-        badgetoggle.style.display = "none";
-      }
-    }*/
+document.getElementById("Laest").innerHTML = lockedbadges;
+document.getElementById("Optjente").innerHTML = unlockedbadges;
 
     function openPage(pageName,elmnt,color) {
       let i, tabcontent, tablinks;
