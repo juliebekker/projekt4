15a793db77d979a371157ad2b262a5914b552e76
AndreaLyng <andrealyng01@gmail.com>
2022-03-14 20:56:45 +0100

    Done

--- javatest.js
+++ javatest.js
@@ -1,19 +1,3 @@
-document.getElementById('belønning').style.visibility='hidden';
-
-
-function myFunction() {
-  let x = document.getElementById("nummer").value;
-  let text;
-  if (isNaN(x) || x < 7 || x > 203) {
-    document.getElementById('belønning').style.visibility='hidden';
-    text = "Koden er ikke gyldig";
-  } else {
-    document.getElementById('belønning').style.visibility='visible';
-    text = "Koden er korrekt";
-  }
-}
-
-
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
